0da1d2005f23b32a2abefe7c4f0f4bf34147f58f
liamH47 <liamhanafee@gmail.com>
2020-11-11 23:24:06 -0500

    almost done

--- src/index.js
+++ src/index.js
@@ -1,130 +1,108 @@
-// Dom Elements
-const quoteForm = document.querySelector("#new-quote-form")
-const quoteList = document.querySelector("#quote-list")
+//dom elements
+window.addEventListener("DOMContentLoaded", event => {
+    console.log("dom loaded")
+    getAllQuotes()
 
 
-function renderAllQuotes(quoteArray) {
-    quoteArray.forEach(quote => {
-        renderOneQuote(quote, quote.likes.length)
-    })
-}
-
-function renderOneQuote(quoteObj, likes) {
-    const author = quoteObj.author
-    const quote = quoteObj.quote
-    const li = createLi(quoteObj.id)
-    li.innerHTML = `
-        <blockquote class="blockquote">
-            <p class="mb-0">
-                ${quote}
-            </p>
-            <footer class="blockquote-footer">
-                ${author}
-            </footer>
-                <br>
-            <button class='btn-success'>Likes: <span>${likes}</span></button>
-            <button class='btn-danger'>Delete</button>
-        </blockquote>
-    `
-    quoteList.append(li)
-}
-
-const createLi = id => {
-    let li = document.createElement('li')
-    li.className = 'quote-card'
-    li.dataset.id = id
-    return li
-}
-
-function initialize() {
-    fetch('http://localhost:3000/quotes?_embed=likes')
-    .then(response => response.json())
-    .then(quoteArray => {
-        console.log(quoteArray)
-        renderAllQuotes(quoteArray)
-    })
-}   
-
-//Create Quote 
-const submitHandler = () => {
-    quoteForm.addEventListener('submit', e => {
-        e.preventDefault()
-        const quote = e.target.quote.value
-        const author = e.target.author.value
-        console.log(quote, author)
-        postNewAuthor(quote, author)
-        e.target.reset()
-    })
-}
-    
-
-const postNewAuthor = (quote, author) => {
-    fetch('http://localhost:3000/quotes', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        body: JSON.stringify({
-            quote: quote,
-            author: author,
-            created_at: Date.now()
-        })
-    })
-    .then(response => response.json())
-    .then(obj => {
-        renderOneQuote(obj, 0)
-    })
-}
-
-const clickHandler = () => {
-    document.addEventListener('click', e => {
-         if (e.target.matches('.btn-danger')) {
-            const deleteBtn = e.target
-            const quoteCard = deleteBtn.closest('li')
-            deleteQuote(quoteCard) 
-        } else if (e.target.matches('.btn-success')) {
-            addLike(e)
-        }
-    })
-}
-
-const addLike = e => {
-    const li = e.target.closest('li')
-    const quoteId = parseInt(li.dataset.id)
-    const span = e.target.firstElementChild
-    const newLikeTotal = parseint(span.textContent) + 1
-}
-
-const data = {
-    quoteId: quoteId
-    // createdt: Math.floor(Date.now()/1000)
-}
-
-const config = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-}
-
-fetch('http://localhost/3000/likes', config)
-.then(response => response.json())
-.then(() => {
-    span.textContent = newLikeTotal
 })
 
-const deleteQuote = quoteCard => {
-    const id = quoteCard.dataset.id
-    fetch('http://localhost:/3000/quotes/${id}', {
-        method: 'DELETE',
-    })
-    .then(response => response.json())
-    .then(whatever => quoteCard.remove())
-}
+const quoteListUl = document.querySelector("#quote-list")
+const quoteForm = document.querySelector("#new-quote-form")
+
+
 
 //render functions
-clickHandler()
-submitHandler()
-initialize()
+const renderAllquotes = quoteArray => {
+    quoteArray.forEach(quoteObj => renderOneQuote(quoteObj))
+}
+
+const renderOneQuote = quoteObj => {
+    const quoteCard = document.createElement('li')
+    quoteCard.className = "quote-card"
+    quoteCard.innerHTML = `
+    <blockquote class="blockquote">
+    <p class="mb-0">${quoteObj.quote}</p>
+    <footer class="blockquote-footer">${quoteObj.author}</footer>
+    <br>
+    <button class='btn-success'>Likes: <span class='likes-span'>0</span></button>
+    <button class='btn-danger'>Delete</button>
+  </blockquote>
+    `
+    quoteCard.dataset.id = quoteObj.id
+
+    quoteListUl.append(quoteCard)
+    // let deleteBtn = document.querySelector(".btn-danger").closest
+    // deleteBtn.addEventListener("click", event => {
+    //     console.log(event)
+    // })
+}
+
+
+
+
+
+// event handlers
+
+quoteForm.addEventListener("submit", event => {
+    event.preventDefault()
+    console.log(event)
+
+    const quoteObj = {
+        quote: event.target.quote.value,
+        author: event.target.author.value
+    }
+    renderOneQuote(quoteObj)
+})
+
+quoteListUl.addEventListener("click", function(event) {
+    if (event.target.matches(".btn-danger")) {
+        const button = event.target
+        const card = button.closest(".quote-card")
+        const id = parseInt(card.dataset.id)
+        deleteById(id)
+        card.remove()
+    } else if (event.target.matches(".btn-success")) {
+        const button = event.target
+        const card = button.closest(".quote-card")
+        const id = parseInt(card.dataset.id)
+        debugger
+        // const likesSpan = button.closest(".likes-span")
+        // likesSpan.textContent = parseInt(likesSpan.textContent) + 1
+        // likeFetch()
+    }
+})
+
+
+// fetches
+const likeFetch = () => {
+    fetch('http://localhost:3000/likes', {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            quoteId: id
+        })
+    })
+}
+
+const deleteById = id => {
+
+    fetch(`http://localhost:3000/quotes/${id}`, {
+        method: 'DELETE',
+    })
+    .then(r => r.json())
+    .then(data => console.log(data))
+}
+
+const getAllQuotes = () => {
+    fetch('http://localhost:3000/quotes')
+    .then(r => r.json())
+    .then(quoteArray => renderAllquotes(quoteArray))
+}
+
+const getQuoteById = id => {
+    fetch(`http://localhost:3000/quotes/${id}`)
+    .then(r => r.json())
+    .then(quoteObj => renderOneQuote(quoteObj))
+}
